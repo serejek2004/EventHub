e7423f5a14bf0ab7d66db857b88f3bfbafa29e72
serejek2004 <serejek2004@gmail.com>
2025-02-25 22:25:11 +0200

    Bug fix & update .gitignore

--- event_hub/event_hub/settings.py
+++ event_hub/event_hub/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-iifjf=xzaxc80kq+dhz!(pf7c&9%3f6zv@kj_ao$lcwp)!9l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- event_hub/profile/forms.py
+++ event_hub/profile/forms.py
@@ -1,21 +1,27 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 
 class UserRegistrationForm(forms.Form):
 
     username = forms.CharField(
+        min_length=4,
         max_length=30,
         required=True,
+        validators=[RegexValidator(r'^(?!\.)([a-zA-Z0-9_.]+)(?<!\.)$', 'User name cannot start and end by .')],
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': 'Username',
+
         })
     )
 
     first_name = forms.CharField(
+        min_length=2,
         max_length=30,
         required=True,
+        validators=[RegexValidator(r'([a-zA-Z]+)', 'First name can have only a-z.')],
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': 'First name',
@@ -23,8 +29,10 @@ class UserRegistrationForm(forms.Form):
     )
 
     last_name = forms.CharField(
+        min_length=2,
         max_length=30,
         required=True,
+        validators=[RegexValidator(r'([a-zA-Z]+)', 'Last name can have only a-z.')],
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'placeholder': 'Last name',
@@ -32,6 +40,7 @@ class UserRegistrationForm(forms.Form):
     )
 
     email = forms.EmailField(
+        min_length=6,
         max_length=50,
         required=True,
         widget=forms.EmailInput(attrs={
@@ -41,6 +50,7 @@ class UserRegistrationForm(forms.Form):
     )
 
     password = forms.CharField(
+        min_length=8,
         max_length=50,
         required=True,
         widget=forms.PasswordInput(attrs={
@@ -51,6 +61,7 @@ class UserRegistrationForm(forms.Form):
     )
 
     password_confirm = forms.CharField(
+        min_length=8,
         max_length=50,
         required=True,
         widget=forms.PasswordInput(attrs={
@@ -63,16 +74,20 @@ class UserRegistrationForm(forms.Form):
     def clean(self):
         cleaned_data = super().clean()
         username = cleaned_data.get('username')
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
         email = cleaned_data.get('email')
         password = cleaned_data.get("password")
         password_confirm = cleaned_data.get("password_confirm")
 
-        if username.lower() and User.objects.filter(username=username).exists():
+        if username and User.objects.filter(username=username).exists():
             raise forms.ValidationError("Username already in use.")
+        if len(first_name) < 2:
+            raise forms.ValidationError("First name must be longer than 1 symbol")
+        if len(last_name) < 2:
+            raise forms.ValidationError("Last name must be longer than 1 symbol")
         if email and User.objects.filter(email=email).exists():
             raise forms.ValidationError("Email already in use.")
-        if len(password) < 8:
-            raise forms.ValidationError("Password must be at least 8 characters long.")
         if password and password_confirm and password != password_confirm:
             raise forms.ValidationError("Passwords do not match.")
 
@@ -89,8 +104,10 @@ class UserRegistrationForm(forms.Form):
         )
         return user
 
+
 class UserLoginForm(forms.Form):
     username = forms.CharField(
+        min_length=4,
         max_length=30,
         required=True,
         widget=forms.TextInput(attrs={
@@ -99,6 +116,7 @@ class UserLoginForm(forms.Form):
         })
     )
     password = forms.CharField(
+        min_length=8,
         max_length=50,
         required=True,
         widget=forms.PasswordInput(attrs={
@@ -110,6 +128,7 @@ class UserLoginForm(forms.Form):
 
 class UserProfileInfoUpdateForm(forms.Form):
     first_name = forms.CharField(
+        min_length=2,
         max_length=30,
         required=True,
         widget=forms.TextInput(attrs={
@@ -118,6 +137,7 @@ class UserProfileInfoUpdateForm(forms.Form):
         })
     )
     last_name = forms.CharField(
+        min_length=2,
         max_length=30,
         required=True,
         widget=forms.TextInput(attrs={
@@ -126,6 +146,7 @@ class UserProfileInfoUpdateForm(forms.Form):
         })
     )
     email = forms.EmailField(
+        min_length=6,
         max_length=50,
         required=True,
         widget=forms.EmailInput(attrs={
@@ -159,10 +180,13 @@ class UserProfileInfoUpdateForm(forms.Form):
     def clean(self):
         cleaned_data = super().clean()
         email = cleaned_data.get('email')
+        age = cleaned_data.get('age')
 
         if email:
             if User.objects.filter(email=email).exclude(id=self.user.id).exists():
                 raise forms.ValidationError("This email is already in use by another user.")
+        if age < 12:
+            raise forms.ValidationError('Minimal user age is 12 y.o.')
 
         return cleaned_data
 
@@ -187,6 +211,7 @@ class UserProfileInfoUpdateForm(forms.Form):
 class PasswordUpdateForm(forms.Form):
     old_password = forms.CharField(
         max_length=50,
+        min_length=8,
         required=True,
         widget=forms.PasswordInput(attrs={
             'class': 'form-control',
@@ -195,6 +220,7 @@ class PasswordUpdateForm(forms.Form):
     )
     new_password = forms.CharField(
         max_length=50,
+        min_length=8,
         required=True,
         widget=forms.PasswordInput(attrs={
             'class': 'form-control',
@@ -203,6 +229,7 @@ class PasswordUpdateForm(forms.Form):
     )
     new_password_confirm = forms.CharField(
         max_length=50,
+        min_length=8,
         required=True,
         widget=forms.PasswordInput(attrs={
             'class': 'form-control',
@@ -219,9 +246,6 @@ class PasswordUpdateForm(forms.Form):
         old_password = cleaned_data.get('old_password')
         new_password = cleaned_data.get('new_password')
         new_password_confirm = cleaned_data.get('new_password_confirm')
-
-        if len(new_password) < 8:
-            raise forms.ValidationError("New password must be at least 8 characters.")
 
         if new_password and new_password_confirm and new_password != new_password_confirm:
             raise forms.ValidationError("Passwords do not match.")
